a7ecd1511e6e8256a8a6f37fc5df93601fc39487
deepakgitoffical <aiice250609@gmail.com>
2024-12-01 22:58:02 +0530

    first commit

--- src/GitUserApi.jsx
+++ src/GitUserApi.jsx
@@ -3,22 +3,22 @@ import { useState, useEffect } from 'react'
 import Card from './Card'
 const GitUserApi = () => {
 
-    const [data, setData] = useState([])
+  const [data, setData] = useState([])
 
-    const getUser = async () =>{
-        try {
-            const apiResponse =await fetch("https://api.github.com/users");
-            const result = await  apiResponse.json();
-            setData(result);
-        } catch (error) {
-            console.log(error, "gituser error")
-        }
-        // fetch("https://api.github.com/users").then((result) => {
-        //     result.json().then((resp) => {
-        //       setData(resp)
-        //     })
-        //   })
+  const getUser = async () => {
+    try {
+      const apiResponse = await fetch("https://api.github.com/users");
+      const result = await apiResponse.json();
+      setData(result);
+    } catch (error) {
+      console.log(error, "gituser error")
     }
+    // fetch("https://api.github.com/users").then((result) => {
+    //     result.json().then((resp) => {
+    //       setData(resp)
+    //     })
+    //   })
+  }
 
   useEffect(() => {
 
@@ -27,19 +27,17 @@ const GitUserApi = () => {
 
   return (
     <div className='allCards'>
-{
+      {
         data.map((item) => <div>
-        
-          <Card  name ={item.login}
-           id={item.id} 
-           dp={item.avatar_url}
-           url= {item.html_url}
-
-           />
+          <Card name={item.login}
+            id={item.id}
+            dp={item.avatar_url}
+            url={item.html_url}
+          />
         </div>
         )
       }
-      
+
     </div>
   )
 }
